c2cb8402aa87e5567efc03bf2df1691482442a76
hef <hef@pbrfrat.com>
2011-12-12 23:50:21 -0600

    Initial Maven repository setup

---
+++ src/main/java/edu/uic/cs/acm/WandsNStuff/WandsNStuff.java
@@ -0,0 +1,15 @@
+package edu.uic.cs.acm.WandsNStuff;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WandsNStuff extends JavaPlugin{
+
+    public void onDisable() {
+
+    }
+
+    public void onEnable() {
+
+    }
+    
+}
